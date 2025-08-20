33d352cc90e062ee2321e64c6b7b53fb6d1d6776
kavita <kavitasoren2000@gmail.com>
2025-08-20 13:19:31 +0530

    edit-readme

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -23,7 +23,7 @@ const Contact: React.FC = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:5000/send/email", {
+      const response = await fetch("https://kavita-portfolio.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
