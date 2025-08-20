2e33a3cf009a06435c48651507a6f553747d21ad
kavita <kavitasoren2000@gmail.com>
2025-08-20 22:42:22 +0530

    my-portfolio

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -23,7 +23,7 @@ const Contact: React.FC = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await fetch("https://kavita-portfolio.onrender.com", {
+      const response = await fetch("https://kavita-portfolio.onrender.com/send/email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
