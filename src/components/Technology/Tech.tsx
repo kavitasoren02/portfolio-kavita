93b79eacb6523f50138a51bcad2565712b8e52f5
kavita <kavitasoren2000@gmail.com>
2025-08-21 10:57:24 +0530

    my-portfolio

--- src/components/Technology/Tech.tsx
+++ src/components/Technology/Tech.tsx
@@ -4,11 +4,15 @@ import BallCanvas from './Ball';
 import SectionWrapper  from '../../hoc/SectionWrapper';
 import { techLink, type TechLink } from '../../constants';
 
+const isAndroid = /Android/i.test(navigator.userAgent);
+
+const filteredTechLinks = isAndroid ? techLink.slice(0, 7) : techLink;
+
 const Tech: React.FC = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-        {techLink.map((tech: TechLink) => (
-            <div className='w-28 h-28' key={tech.icon}>
+        {filteredTechLinks.map((tech: TechLink) => (
+            <div className='w-28 h-28' key={tech.name}>
                 <BallCanvas icon={tech.icon} />
                 </div>
         ))}
